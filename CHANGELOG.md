3f0ce78083d395a859002372bdae71844b3d2f30
Curtis Timson <curt@live.co.uk>
2019-07-14 21:09:07 +0100

    Plus Codes Integration (#7)
    * Add check in view
    
    * Refactor navigation
    
    * Add Open Location Code
    
    * Add Map
    
    * Add leaflet css
    
    * Update change log

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 `0.1.0-dev`
 # Minor Changes
  - Add React Bootstrap
+ - Plus Codes Integration
  
 # Patches
  - Netlify redirects file to fix auth callback
--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux'
 import ReactGA from 'react-ga';
-import Login from './components/Login';
-import Logout from './components/Logout';
 import history from './history';
 import AuthCallback from './components/AuthCallback';
+import Home from './views/Home';
+import Location from './views/Location';
 import config from './config';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import Header from './components/Header';
 
 function App(props) {
 
@@ -19,28 +18,14 @@ function App(props) {
   ReactGA.pageview(window.location.pathname + window.location.search);
 
   return (
-    <Router history={history}>
-      <Route exact path="/" render={() => (
-        <div>
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand>Gridchek</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav>
-                <Nav.Link href="#deets">
-                  {
-                    !props.auth.isAuthenticated ?
-                    <Login/>
-                    : <Logout/>
-                  }
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
-      )} />
-      <Route exact path="/auth-callback" component={AuthCallback} />
-    </Router>
+    <div>
+      <Header/>
+      <Router history={history}>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/auth-callback" component={AuthCallback} />
+        <Route exact path="/l/:code" component={Location} />
+      </Router>
+    </div>
   );
 }
 
