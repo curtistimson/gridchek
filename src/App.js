4d550f2c95523ef549be8bbc48c3060444210f31
Curtis Timson <curt@live.co.uk>
2019-07-14 19:13:22 +0100

    Add check in view

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import Login from './components/Login';
 import Logout from './components/Logout';
 import history from './history';
 import AuthCallback from './components/AuthCallback';
+import CheckIn from './views/CheckIn';
 import config from './config';
+import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App(props) {
 
@@ -19,28 +20,32 @@ function App(props) {
   ReactGA.pageview(window.location.pathname + window.location.search);
 
   return (
-    <Router history={history}>
-      <Route exact path="/" render={() => (
-        <div>
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand>Gridchek</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav>
-                <Nav.Link href="#deets">
-                  {
-                    !props.auth.isAuthenticated ?
-                    <Login/>
-                    : <Logout/>
-                  }
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
-      )} />
-      <Route exact path="/auth-callback" component={AuthCallback} />
-    </Router>
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href='/'>Gridchek</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav>
+            <Nav.Link href="#deets">
+              {
+                !props.auth.isAuthenticated ?
+                <Login/>
+                : <Logout/>
+              }
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Router history={history}>
+        <Route exact path="/" render={() => (
+          <div>
+            <Button href='/checkin'>Check In</Button>
+          </div>
+        )} />
+        <Route exact path="/auth-callback" component={AuthCallback} />
+        <Route exact path="/checkin" component={CheckIn} />
+      </Router>
+    </div>
   );
 }
 
---
+++ src/views/CheckIn/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CheckIn extends Component {
+    render() {
+        return (
+          <div>Check In</div>
+        )
+      }
+}
+
+export default CheckIn;
