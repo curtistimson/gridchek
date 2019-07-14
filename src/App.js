d30ad471301034aec2e323fa9c94f0dff37bffcd
Curtis Timson <curt@live.co.uk>
2019-07-14 18:58:51 +0100

    React Bootstrap (#6)
    * Add navbar
    
    * Add Button
    
    * Update change log

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux'
 import ReactGA from 'react-ga';
-import './App.css';
 import Login from './components/Login';
 import Logout from './components/Logout';
 import history from './history';
 import AuthCallback from './components/AuthCallback';
 import config from './config';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App(props) {
 
@@ -19,14 +21,22 @@ function App(props) {
   return (
     <Router history={history}>
       <Route exact path="/" render={() => (
-        <div className="App">
-          <header className="App-header">
-            {
-              !props.auth.isAuthenticated ?
-              <Login/>
-              : <Logout/>
-            }
-          </header>
+        <div>
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand>Gridchek</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav>
+                <Nav.Link href="#deets">
+                  {
+                    !props.auth.isAuthenticated ?
+                    <Login/>
+                    : <Logout/>
+                  }
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
         </div>
       )} />
       <Route exact path="/auth-callback" component={AuthCallback} />
