6237df31b3703d5a5c91c18ed83656b5acfb225d
Curtis Timson <curt@live.co.uk>
2019-07-25 18:43:15 +0100

    Remove redundant import

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
-import { OpenLocationCode } from 'open-location-code';
 import history from '../../history';
 
 class Home extends Component {
