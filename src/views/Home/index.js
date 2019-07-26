b79a2969ee280f75c5999711b5a5404180b47b9b
Curtis Timson <curt@live.co.uk>
2019-07-26 16:52:14 +0100

    Lint on home and header

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,48 +1,67 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
-import history from '../../history';
-import { fetchUserCheckins } from '../../actions/checkinActions';
+import PropTypes from 'prop-types';
 
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 
+import { fetchUserCheckins } from '../../actions/checkinActions';
+import history from '../../history';
+
 class Home extends Component {
-    componentDidMount() {
-      this.props.dispatch(fetchUserCheckins());
-    }
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(fetchUserCheckins());
+  }
 
-    render() {
+  render() {
+    const locationRedirect = () => {
+      history.replace('/checkin');
+    };
 
-      const locationRedirect = () => {
-        history.replace(`/checkin`);
-      }
+    const { userCheckins } = this.props;
 
-      return (
-          <div>
-            <div>
-                <Button onClick={locationRedirect}>Check In</Button>
-            </div>
-            <h2>{this.props.userCheckins.count} Checkins</h2>
-            <Row>
-              {
-                this.props.userCheckins.checkins && this.props.userCheckins.checkins.map(checkin => (
-                  <Col sm={4}>
-                    <Card style={{ marginTop: '1em' }}>
-                      <Card.Body>
-                          <Card.Title>{checkin.plusCode}</Card.Title>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                ))
-              }
-              </Row>
-          </div>
-      )
+    const {
+      count,
+      checkins,
+    } = userCheckins;
+
+    return (
+      <div>
+        <div>
+          <Button onClick={locationRedirect}>Check In</Button>
+        </div>
+        <h2>
+          {count}
+          Checkins
+        </h2>
+        <Row>
+          {
+            checkins && checkins.map(checkin => (
+              <Col sm={4}>
+                <Card style={{ marginTop: '1em' }}>
+                  <Card.Body>
+                    <Card.Title>{checkin.plusCode}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+          }
+        </Row>
+      </div>
+    );
   }
 }
+
+Home.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  userCheckins: PropTypes.shape({
+    count: PropTypes.number,
+    checkins: PropTypes.array,
+  }).isRequired,
+};
 
 export default connect(store => ({
   userCheckins: store.userCheckins,
